1f95b747901d7be794cf87c9e04db323cd5dca45
Hesennov <hasanhasanov345@gmail.com>
2026-03-16 23:18:10 -0600

    fix:userSlice

--- src/store/users/userSlice.ts
+++ src/store/users/userSlice.ts
@@ -1,16 +1,61 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import apiClient from "../../api/client";
+import type { User, UpdateUserData } from "../../types/user";
+import { userService } from "../../services/userServices";
 
 interface UsersState {
-    data:string[]
+  users: User[];
+  loading: boolean;
+  error: null | null;
+  page: number;
+  totalPages: number;
+  search: string;
 }
 
-const initialState :UsersState ={
-    data:[]
-}
+
+export const fetchUser = createAsyncThunk( 
+    "users/fetchAll",
+    async ( params:{page:number,search:string},{rejectWithValue})=>{
+        try{
+            const response = await userService.getAll({...params,limit:10})
+            return response
+        }catch(error){
+            return rejectWithValue("Users not uploaded")
+        }
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const initialState: UsersState = {
+  users: [],
+  loading: false,
+  error: null,
+  page: 1,
+  totalPages: 10,
+  search: "",
+};
+
 const usersSlice = createSlice({
-    name:"users",
-    initialState,
-    reducers:{}
-})
+  name: "users",
+  initialState,
+  reducers: {},
+});
 
-export default usersSlice.reducer
+export default usersSlice.reducer;
