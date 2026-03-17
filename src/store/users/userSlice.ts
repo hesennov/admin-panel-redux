9ae0e8adeb706f88df4c10778c60a4890e28cecb
Hesennov <hasanhasanov345@gmail.com>
2026-03-17 00:26:52 -0600

    end:userSlice

--- src/store/users/userSlice.ts
+++ src/store/users/userSlice.ts
@@ -6,42 +6,49 @@ import { userService } from "../../services/userServices";
 interface UsersState {
   users: User[];
   loading: boolean;
-  error: null | null;
+  error: null | string;
   page: number;
   totalPages: number;
   search: string;
 }
 
+export const fetchUser = createAsyncThunk(
+  "users/fetchAll",
+  async (params: { page: number; search: string }, { rejectWithValue }) => {
+    try {
+      const response = await userService.getAll({ ...params, limit: 10 });
+      return response;
+    } catch (error) {
+      return rejectWithValue("Users not uploaded");
+    }
+  },
+);
 
-export const fetchUser = createAsyncThunk( 
-    "users/fetchAll",
-    async ( params:{page:number,search:string},{rejectWithValue})=>{
-        try{
-            const response = await userService.getAll({...params,limit:10})
-            return response
-        }catch(error){
-            return rejectWithValue("Users not uploaded")
-        }
-
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export const deleteUser = createAsyncThunk(
+  "users/delete",
+  async (id: number, { rejectWithValue }) => {
+    try {
+        await userService.delete(id);
+      return id;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
+export const editUser = createAsyncThunk(
+  "users/edit",
+  async (
+    { id, data }: { id: number; data: UpdateUserData },
+    { rejectWithValue },
+  ) => {
+    try {
+      const response = await userService.update(id, data);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
 
 const initialState: UsersState = {
   users: [],
@@ -55,7 +62,36 @@ const initialState: UsersState = {
 const usersSlice = createSlice({
   name: "users",
   initialState,
-  reducers: {},
+  reducers: {
+    setPage: (state, action) => {
+      state.page = action.payload;
+    },
+
+    setSearch: (state, action) => {
+      ((state.search = action.payload), (state.page = 1));
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+    .addCase(fetchUser.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    }).addCase(fetchUser.fulfilled,(state,action)=>{
+        state.loading = false
+        state.users = action.payload.data
+        state.totalPages = action.payload.totalPages
+    }).addCase(fetchUser.rejected,(state,action)=>{
+        state.loading = false
+        state.error = action.payload as string
+    }).addCase(deleteUser.fulfilled,(state,action)=>{
+        state.users = state.users.filter((u)=>u.id !== action.payload)
+    }).addCase(editUser.fulfilled, ({users},{payload})=>{
+        const index = users.findIndex((u)=>u.id ===payload.id)
+        if(index !== -1) users[index] = payload;
+    })
+  },
 });
 
+export const {setPage,setSearch} = usersSlice.actions
 export default usersSlice.reducer;
